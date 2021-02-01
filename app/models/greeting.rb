4b8aa6411dda68af161e9df38432e04620d11833
Hannah Vivian Shaw <hvivianshaw@gmail.com>
2021-02-01 04:34:57 -0500

    seed random T words to database

--- app/models/greeting.rb
+++ app/models/greeting.rb
@@ -1,2 +1,12 @@
 class Greeting < ApplicationRecord
+  before_validation :assign_random_word
+
+  validates :word, presence: true
+
+  private
+
+  def assign_random_word
+    random_t_word = TWord.order(Arel.sql('RANDOM()')).first
+    self.word = random_t_word.word
+  end
 end
