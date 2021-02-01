2aeea33075b7519f48908d6ef5bf7fd3f8e97a49
Hannah Vivian Shaw <hvivianshaw@gmail.com>
2021-02-01 14:29:26 -0500

    validate TWord model to ensure words indeed start with T

--- app/models/t_word.rb
+++ app/models/t_word.rb
@@ -1,2 +1,8 @@
 class TWord < ApplicationRecord
+  validates :word, presence: true
+  validate :word_starts_with_t
+
+  def word_starts_with_t
+    errors.add(:word, "That word doesn't start with T!") unless word.start_with?("T", "t")
+  end
 end
