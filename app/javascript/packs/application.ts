d9f0fd94b50e606fd3175d030259a7af5c457c7b
Hannah Vivian Shaw <hvivianshaw@gmail.com>
2021-02-04 03:56:54 -0500

    add basic PWA features

--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -21,3 +21,26 @@ import { definitionsFromContext } from "stimulus/webpack-helpers";
 const application = Application.start();
 const context = require.context("../controllers/", true, /\.ts$/);
 application.load(definitionsFromContext(context));
+
+window.addEventListener("load", () => {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then((registration) => {
+      console.log("ServiceWorker registered: ", registration);
+
+      var serviceWorker;
+      if (registration.installing) {
+        serviceWorker = registration.installing;
+        console.log("Service worker installing.");
+      } else if (registration.waiting) {
+        serviceWorker = registration.waiting;
+        console.log("Service worker installed & waiting.");
+      } else if (registration.active) {
+        serviceWorker = registration.active;
+        console.log("Service worker active.");
+      }
+    })
+    .catch((registrationError) => {
+      console.log("Service worker registration failed: ", registrationError);
+    });
+});
